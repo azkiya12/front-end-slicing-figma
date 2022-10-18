3c882098cabe4de9018037a0d2541c01bd9421a9
azkiya12 <gakrohlali@gmail.com>
2022-10-18 08:01:58 +0700

    perbaikan navbar background scroll

--- script/navbar-scroll.js
+++ script/navbar-scroll.js
@@ -1,6 +1,6 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $new.height());
-    })
-})
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
